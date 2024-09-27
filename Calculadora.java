ce8abb517e26796d0bf02d4180c7d56d3ac84618
Yona <115572178+EliacidF@users.noreply.github.com>
2024-09-27 20:31:34 +0200

    Segunda activida miembro 1 añadir multiplicacion y cambiar suma

---
+++ Calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora {
+    public int suma(int a, int b) { 
+    if (!Utilidades.esNumeroValido(a) || !Utilidades.esNumeroValido(b)) { throw new IllegalArgumentException("Parámetros inválidos"); } return a + b; 
+    }
+
+    public int multiplicacion(int a, int b) { 
+    return a * b; 
+    }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+}
